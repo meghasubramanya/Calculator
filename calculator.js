d4057e9f736c5f17dd499efddefb7cdc2d909948
Megha Subramanya <56509196+meghasubramanya@users.noreply.github.com>
2020-07-10 13:48:23 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,75 +1,98 @@
-let running = 0;
-let buffer = "0";
-let previewsOperator;
-let equal = false;
-const screen = document.querySelector(".screen");
-document.querySelector(".calc-body").addEventListener("click",function(event){
-    ButtonClick(event.target.innerText);
+let runningTotal=0;//total res
+let buffer="0";//waits for the user input
+let previousOperator; //holds the prev button clicked to gv the final ans
+const screen=document.querySelector(".screen");
+
+document.querySelector(".calc-buttons").addEventListener("click", function(event){
+buttonClick(event.target.innerText)
 });
-function ButtonClick(value){
-    if(isNaN(parseInt(value))){
-        HandleSymbol(value);
-    }
-    else{
-        if(equal){
-            equal = false;
-            buffer = "0";
-            rerender();
-        }
-        HandleNumber(value);
-    }
+
+function buttonClick(value){
+    console.log(value);
+if(isNaN(parseInt(value)))
+{
+    handleSymbol(value);
 }
-function HandleSymbol(value){
-    if(value === "C"){
+else{
+    handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value){
+if(buffer==="0")
+{
+    buffer=value;
+}
+else{
+    buffer+=value;
+}
+
+}
+function handleMath(value) {
+    if (buffer === "0") {
+      // do nothing
+      return;
+    }
+  
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+      runningTotal = intBuffer;
+    } else {
+      flushOperation(intBuffer);
+    }
+  
+    previousOperator = value;
+  
+    buffer = "0";
+  }
+  
+  function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+      runningTotal += intBuffer;
+    } else if (previousOperator === "-") {
+      runningTotal -= intBuffer;
+    } else if (previousOperator === "×") {
+      runningTotal *= intBuffer;
+    } else {
+      runningTotal /= intBuffer;
+    }
+    
+  }
+
+function handleSymbol(value) {
+    switch (value) {
+      case "C":
         buffer = "0";
-    }
-    else if (value === "←"){
-        if(buffer.length == 1){
-            buffer = "0";
+        runningTotal = 0;
+        break;
+      case "=":
+        if (previousOperator === null) {
+          // need two numbers to do math
+          return;
         }
-        else{
-            buffer = buffer.substring(0,buffer.length - 1);
+        flushOperation(parseInt(buffer));
+        previousOperator = null;
+        buffer = +runningTotal;
+        runningTotal = 0;
+        break;
+      case "←":
+        if (buffer.length === 1) {
+          buffer = "0";
+        } else {
+          buffer = buffer.substring(0, buffer.length - 1);
         }
+        break;
+      case "+":
+      case "-":
+      case "×":
+      case "÷":
+        handleMath(value);
+        break;
     }
-    else if(value === "="){
-        equal = true;
-        switch(previewsOperator){
-            case '+': buffer = running + parseInt(buffer);
-            break;
-            case '-':buffer =  running - parseInt(buffer); 
-            break;
-            case 'x': buffer = running * parseInt(buffer);
-            break;
-            case '÷': if(parseInt(buffer) !== 0){
-                buffer = running / buffer;
-            }
-            else{
-                alert("you can't divide by 0 or math error");
-            }
-            break;
-        }
-    }
-    else{
-        if(value !== "←"){
-            previewsOperator = value;
-            running = parseInt(buffer);
-            buffer = "0";
-        }
-        else{
-            previewsOperator = "0";
-        }
-    }
-    rerender();
-}
-function HandleNumber(value){
-    if(buffer === "0"){
-        buffer = value;
-    }
-    else{
-        buffer += value;
-    }
-    rerender();
-}
-function rerender(){
-    screen.innerText = buffer;
+  }
+  
+function rerender()
+{
+    screen.innerText=buffer;
 }
